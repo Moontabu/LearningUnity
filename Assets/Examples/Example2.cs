22d3a230749330ccbfc08a307859b79fedd14680
gouzhun <gouzhun@libii.com>
2021-03-20 17:29:49 +0800

    22

--- Assets/Examples/Example2.cs
+++ Assets/Examples/Example2.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestJJJ : MonoBehaviour
+public class Example2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
